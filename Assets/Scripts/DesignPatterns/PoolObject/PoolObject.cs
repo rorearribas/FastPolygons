6f14460aae2d8aaaa0c77d841c43b5423040241b
Rubén Orea Arribas <64693586+rorearribas@users.noreply.github.com>
2023-01-23 22:58:13 +0100

    pool object

--- Assets/Scripts/DesignPatterns/PoolObject/PoolObject.cs
+++ Assets/Scripts/DesignPatterns/PoolObject/PoolObject.cs
@@ -6,49 +6,54 @@ namespace FastPolygons
 {
     public class PoolObject : MonoBehaviour
     {
-        private GameObject m_prefab;
-        private int m_size;
-        private IObjectPool<GameObject> m_objectPool;
+        private readonly GameObject m_prefab;
+        private readonly int m_size;
+        public readonly IObjectPool<GameObject> m_objectPool;
 
         public PoolObject(GameObject prefab, int size)
         {
-            this.m_size = size;
-            this.m_prefab = prefab;
+            m_size = size;
+            m_prefab = prefab;
+
+            m_objectPool = new UnityEngine.Pool.ObjectPool<GameObject>(CreatePooledItem, OnTakeFromPool,
+                OnReturnedToPool, OnDestroyPoolObject, true, Size);
         }
 
-        public int PoolSize { get => m_size; }
-        public IObjectPool<GameObject> Items
-        {
-            get
-            {
-                if (m_objectPool != null) return m_objectPool;
+        public int Size { get => m_size; }
+        public int CountInactive => m_objectPool.CountInactive;
 
-                m_objectPool = new UnityEngine.Pool.ObjectPool<GameObject>(CreatePooledItem, OnTakeFromPool,
-                    OnReturnedToPool, OnDestroyPoolObject, true, PoolSize);
-
-                return m_objectPool;
-            }
-        }
-
-        /* These functions are for the object pool pattern */
         private void OnReturnedToPool(GameObject _Object)
         {
             _Object.SetActive(false);
         }
-
         private void OnTakeFromPool(GameObject _Object)
         {
             _Object.SetActive(true);
         }
-
         private void OnDestroyPoolObject(GameObject _Object)
         {
             Destroy(_Object);
         }
-
         private GameObject CreatePooledItem()
         {
             return Instantiate(m_prefab);
+        }
+
+        public GameObject Get()
+        {
+            return m_objectPool.Get();
+        }
+        public PooledObject<GameObject> Get(out GameObject v)
+        {
+            return m_objectPool.Get(out v);
+        }
+        public void Release(GameObject element)
+        {
+            m_objectPool.Release(element);
+        }
+        public void Clear()
+        {
+            m_objectPool.Clear();
         }
     }
 }
